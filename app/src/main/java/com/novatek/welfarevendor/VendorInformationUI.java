14c246795c6f25091aec3a64caecde8e5af8c72c
MauriceChiu <maurice.chiu@hotmail.com>
2018-09-17 11:26:59 +0800

    Commit: 20180917
    1. check network connection before loading map
    2. make textview (vendor detail) as scrollable
    3. update version/date

--- app/src/main/java/com/novatek/welfarevendor/VendorInformationUI.java
+++ app/src/main/java/com/novatek/welfarevendor/VendorInformationUI.java
@@ -1,11 +1,16 @@
 package com.novatek.welfarevendor;
 
 import android.app.Activity;
+import android.content.Context;
 import android.location.Address;
 import android.location.Geocoder;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.text.method.ScrollingMovementMethod;
 import android.webkit.JavascriptInterface;
 import android.widget.TextView;
 import android.webkit.WebView;
+import android.widget.Toast;
 
 import java.io.IOException;
 import java.util.List;
@@ -45,40 +50,61 @@ public class VendorInformationUI {
 		VendorAddress = (TextView) this.activity.findViewById(R.id.vendor_address);
 		VendorDetail = (TextView) this.activity.findViewById(R.id.vendor_detail);
 
-        VendorMap = (WebView)this.activity.findViewById(R.id.vendor_map);
-        VendorMap.getSettings().setJavaScriptEnabled(true);
-        VendorMap.addJavascriptInterface(JSInterface, "android");
+		if (isConnected()) {
+            VendorMap = (WebView)this.activity.findViewById(R.id.vendor_map);
+            VendorMap.getSettings().setJavaScriptEnabled(true);
+            VendorMap.addJavascriptInterface(JSInterface, "android");
 
-		// transfer address to lat/lng
-        try {
-            CoderAddress = Coder.getFromLocationName(address, 2);
-            CoderLocation = CoderAddress.get(0);
-            JSInterface.setLatitude(CoderLocation.getLatitude());
-            JSInterface.setLongitude(CoderLocation.getLongitude());
+            // transfer address to lat/lng
+            try {
+                CoderAddress = Coder.getFromLocationName(address, 2);
+                CoderLocation = CoderAddress.get(0);
+                JSInterface.setLatitude(CoderLocation.getLatitude());
+                JSInterface.setLongitude(CoderLocation.getLongitude());
+            }
+            catch (IOException ex) {
+                ex.printStackTrace();
+            }
+            // load google map
+            VendorMap.loadUrl("file:///android_asset/GoogleMap.html");
         }
-        catch (IOException ex) {
-            ex.printStackTrace();
+        else {
+            JSInterface.setLatitude(24.771588);
+            JSInterface.setLongitude(121.025196);
+
+            Toast toast = Toast.makeText(this.activity,
+                    R.string.no_internet, Toast.LENGTH_LONG);
+            toast.show();
         }
-        // load google map
-        VendorMap.loadUrl("file:///android_asset/GoogleMap.html");
 
 		VendorName.setText(name);
 		VendorPhone.setText(phone);
 		VendorAddress.setText(address);
 		VendorDetail.setText(detail);
+
+        VendorDetail.setMovementMethod(new ScrollingMovementMethod());
 	}
 	
 	public void SetVendorCategoryUI(String category) {
 		this.activity.setContentView(R.layout.ui_vendorinformation);
 	}
 
+    private boolean isConnected() {
+        ConnectivityManager cm = (ConnectivityManager) this.activity.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = cm.getActiveNetworkInfo();
+        if (networkInfo != null && networkInfo.isConnected()) {
+            return true;
+        }
+        return false;
+    }
+
     private class JavaScriptInterface {
         private double latitude,longitude;
 
         // default address: Novatek
         public JavaScriptInterface() {
-            longitude = 121.025196;
             latitude = 24.771588;
+            longitude = 121.025196;
         }
 
         public void setLatitude(double lat) {
